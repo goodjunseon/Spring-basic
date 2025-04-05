0250c329297e80dafbf249bc6cf63d5238d340ae
goodjunseon <pzs2001926@gmail.com>
2025-04-05 17:35:14 +0900

    싱글톤 완료

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -14,19 +14,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // @Configuration -> 설정 정보
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
+
     @Bean // @Bean -> Spring Container에 Bean 객체 선언
     // 빈 이름 : memberService, 타입 : MemberService
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); // memberService 실행 -> MemberServiceImpl -> MemoryMemberRepository
     }
 
     @Bean
-    private static MemoryMemberRepository memberRepository() {
+    public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
